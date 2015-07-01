d2010c9ba37f316971fd21b446bdb67f94ddb6dd
Chris Kwok <chris.kwok@climate.com>
2015-06-30 20:36:27 -0700

    init project

---
+++ EmdibApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmdibApp
+//
+//  Created by Chris Kwok on 6/30/15.
+//  Copyright (c) 2015 Chris Kwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EmdibApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EmdibApp
+//
+//  Created by Chris Kwok on 6/30/15.
+//  Copyright (c) 2015 Chris Kwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
