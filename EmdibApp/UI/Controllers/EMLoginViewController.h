f2d0d8829a963c562380be81a978b1fc8058f4ad
Chris Kwok <chris.kwok@climate.com>
2015-08-28 01:10:52 -0700

    add new login view controller
    - include fb key in plist

---
+++ EmdibApp/UI/Controllers/EMLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  EMLoginViewController.h
+//  EmdibApp
+//
+//  Created by Chris Kwok on 8/28/15.
+//  Copyright (c) 2015 Chris Kwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMLoginViewController : UIViewController
+
+@end
